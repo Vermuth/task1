92fa8e6464029ce90c67bcf9c430464b2f6c719e
Vermuth <dmsunbox@gmail.com>
2014-03-21 16:35:01 +0400

    finish

---
+++ task1.rb
@@ -0,0 +1,23 @@
+module Enumerable
+  def cluster
+    cluster = []
+    each do |element|
+      if cluster.last && cluster.last.last == element
+       cluster.last << element
+      else
+       cluster << [element]
+      end
+    end
+    cluster
+  end
+end
+def start(number, round)
+  total = []
+  1.upto(round) do
+    number.cluster.map{ |x| total <<  x.uniq.unshift(x.size) }
+    p number = total.flatten!
+    total = []
+  end
+end
+
+start([1], 10)
